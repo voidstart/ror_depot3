93dbe6dc766a65ff45b3ca013f2e30fe1fd26140
Wang Wei <voidstarting@gmail.com>
2013-11-18 14:29:19 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create(title: 'Coffeescript',
+description:
+%{<p>
+CoffeeScript is a little language that compiles into JavaScript. Underneath that awkward Java-esque patina, JavaScript has always had a gorgeous heart. CoffeeScript is an attempt to expose the good parts of JavaScript in a simple way. The golden rule of CoffeeScript is: "It's just JavaScript". The code compiles one-to-one into the equivalent JS, and there is no interpretation at runtime. You can use any existing JavaScript library seamlessly from CoffeeScript (and vice-versa). The compiled output is readable and pretty-printed, will work in every JavaScript runtime, and tends to run as fast or faster than the equivalent handwritten JavaScript.
+</p>},
+image_url: 'http://jashkenas.github.io/coffee-script/documentation/images/logo.png',
+price: 36.99)
+Product.create(title: 'JavaScript',
+description:
+%{<p>
+Web lanaguage
+</p>},
+image_url: 'http://jashkenas.github.io/coffee-script/documentation/images/logo.png',
+price: 36.99)
+
+
