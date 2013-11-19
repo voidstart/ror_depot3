d5e9a8847549dc4d9ba4384a81724e32c00e3fbf
Wang Wei <voidstarting@gmail.com>
2013-11-19 15:20:34 +0800

    test validation of length

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ActiveRecord::Base
 validates :title, :description, :image_url, presence: true
 validates :price, numericality: {greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
+validates :title, length: {minimum: 10,
+ too_short: "Title should be longer!!" }
 validates :image_url, allow_blank: true, format: {
 	with: %r{\.(gif|png|jpg)$}i,
 	message: 'Url must end in gif/png/jpg'
