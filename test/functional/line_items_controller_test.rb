ffb33315e5f94872d48845160fbfbc6b07257871
Wang Wei <voidstarting@gmail.com>
2013-11-26 20:39:03 +0800

    test ajax add line_item

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -23,6 +23,17 @@ class LineItemsControllerTest < ActionController::TestCase
 
     assert_redirected_to store_path
   end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:ruby).id
+    end 
+
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
+  end
 # test duplicate products added to cart
 test "should has same item entries count when add duplicate product" do
 # add one Ruby book
