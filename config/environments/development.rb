748e3b3279f35a9cf0e82480703cbe8de3406a68
Wang Wei <voidstarting@gmail.com>
2013-11-28 11:38:07 +0800

    fix mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,14 +17,14 @@ Depot::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-address: "smtp.gmail.com"
-port: 587,
-domain: "gmail.com",
-authentication: "plain",
-user_name: "voidstart.hi",
-password: "strongpassword1",
-enable_starttls_auto: true
-}
+	address: "smtp.gmail.com",
+	port: 587,
+	domain: "gmail.com",
+	authentication: "plain",
+	user_name: "voidstart.hi",
+	password: "strongpassword1",
+	enable_starttls_auto: true
+	}
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
