666fc69231859a5e60cfcfa45a186a7008ef3918
Wang Wei <voidstarting@gmail.com>
2013-11-28 13:19:56 +0800

    mailing out works

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -54,6 +54,7 @@ end
       if @order.save
     Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+	OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice: 
           'Thank you for your order.' }
         format.json { render json: @order, status: :created,
