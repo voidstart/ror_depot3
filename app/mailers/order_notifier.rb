03a751be1ab3ecd92de0b32efa565efcbaf08e57
Wang Wei <voidstarting@gmail.com>
2013-11-28 15:21:26 +0800

    pass test

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -9,7 +9,7 @@ class OrderNotifier < ActionMailer::Base
   def received(order)
     @order = order 
 
-    mail to: order.email, subject: 'Prgamatic Store Order Confirmation' 
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation' 
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
